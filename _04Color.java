f8079795daf15c4a5659b1d1bc2f896982e4b245
mjdargen <mjdargen@gmail.com>
2023-04-16 22:54:54 -0400

    updated color example

--- _04Color.java
+++ _04Color.java
@@ -8,9 +8,29 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.paint.Color;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.shape.Rectangle;
 
 public class _04Color extends Application {
   public void start(Stage primaryStage) throws Exception {
+
+    // creating colors
+    double red = 1.0;
+    double green = 0.8;
+    double blue = 0.6;
+    double alpha = 1.0;
+    Color color = new Color(red, green, blue, alpha);
+    Color color1 = Color.web("#ff00ff");
+    Color color2 = Color.web("#ff00ff", 0.5);
+    Color color3 = Color.rgb(255, 0, 255);
+    Color color4 = Color.rgb(255, 0, 255, 0.5);
+    Color color5 = Color.grayRgb(255);
+    Color color6 = Color.grayRgb(255, 0.5);
+    Color color7 = Color.hsb(1.0, 0.5, 0.8);
+    Color color8 = Color.hsb(1.0, 0.5, 0.8, 0.5);
+    Color color9 = Color.color(1.0, 0.0, 1.0);
+    Color color10 = Color.color(1.0, 0.0, 1.0, 0.5);
 
     // create color picker
     ColorPicker colorPicker = new ColorPicker();
@@ -28,16 +48,36 @@ public class _04Color extends Application {
           value.getSaturation() + ", " + value.getBrightness() + ")");
     });
 
+    // create a rectangle shape and set the color
+    Rectangle rectangle = new Rectangle();
+    rectangle.setX(200);
+    rectangle.setY(600);
+    rectangle.setWidth(200);
+    rectangle.setHeight(200);
+    rectangle.setStroke(Color.TRANSPARENT);
+    rectangle.setFill(Color.valueOf("#00ffff"));
+
     // create a layout
     VBox layout = new VBox(20);
     layout.setAlignment(Pos.CENTER);
-    layout.getChildren().addAll(colorPicker, rawValue, rgb, opacity, hsb);
+    layout.getChildren().addAll(colorPicker, rawValue, rgb, opacity, hsb, rectangle);
+
+    // set background with background fill
+    Background background = new Background(new BackgroundFill(Color.DARKBLUE, null, null));
+    layout.setBackground(background);
+
+    // set text color
+    rawValue.setTextFill(Color.WHITE);
+    rgb.setTextFill(Color.WHITE);
+    opacity.setTextFill(Color.WHITE);
+    hsb.setTextFill(Color.WHITE);
 
     // set the scene / stage
     Scene scene = new Scene(layout, 400, 800);
     primaryStage.setTitle("Color Example");
     primaryStage.setScene(scene);
     primaryStage.show();
+
   }
 
   public static void main(String[] args) {
